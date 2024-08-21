01b2b769523bb2d07bb8911e0171f852b1fc02ae
bitful-pannul <bitful-pancreas@protonmail.com>
2024-08-21 23:27:07 +0300

    temp(pubsub): even more prints

--- crates/pubsub/src/managers/in_flight.rs
+++ crates/pubsub/src/managers/in_flight.rs
@@ -57,6 +57,7 @@ impl InFlight {
     pub(crate) fn fulfill(self, resp: Response) -> Option<(SubId, Self)> {
         if self.is_subscription() {
             if let ResponsePayload::Success(val) = resp.payload {
+                println!("fullfil value {:?}", val);
                 let sub_id: serde_json::Result<SubId> = serde_json::from_str(val.get());
                 return match sub_id {
                     Ok(alias) => Some((alias, self)),
